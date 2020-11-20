d4e9336b94a813a999823ac6c8b8346c4fb85680
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-11-19 17:26:09 -0700

    Add project files.

---
+++ Exercise10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise10
+{
+    class EndOfWords
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file;
+            string sentence;
+            string[] wordList;
+            int total = 0;
+
+            try
+            {
+                file = File.OpenText("TextFile1.txt");
+                sentence = file.ReadLine();
+                wordList = sentence.Split(' ');
+
+                foreach (string s in wordList)
+                {
+                   if (s.EndsWith('t') || s.EndsWith('e'))
+                    {
+                        total++;
+                    }
+                }
+
+                Console.WriteLine("There are a total of " + total + " words that end with t or e");
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        
+    }
+}
